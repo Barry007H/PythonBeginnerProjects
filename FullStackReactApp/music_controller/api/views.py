18a7b0916397c143f9e00ee605fd7da214160d75
Barry007H <barry7892011@hotmail.co.uk>
2021-07-26 01:27:57 +0100

    Fixed import errors

--- FullStackReactApp/music_controller/api/views.py
+++ FullStackReactApp/music_controller/api/views.py
@@ -1,10 +1,9 @@
-from FullStackReactApp.music_controller.api.models import Room
 from django.shortcuts import render
 from rest_framework import generics
 from .serializers import RoomSerializer
 from .models import Room
 
 # Create your views here.
-class RoomView(generics.CreateAPIView) :
+class RoomView(generics.ListAPIView) :
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
